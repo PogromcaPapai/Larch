18378c07e79e4fb7050cb2a5f855d73f577d1268
Jakub Dakowski <jakubdakowski@gmail.com>
2020-09-02 18:25:28 +0200

    Data structure defined, not yet tested

--- app/engine.py
+++ app/engine.py
@@ -2,6 +2,7 @@ import json
 import logging as log
 import os
 import typing as tp
+from string import ascii_uppercase as alphabet
 
 import pop_engine as pop
 import logging
@@ -26,6 +27,89 @@ def EngineChangeLog(func, *args, **kwargs):
         return func(*args, **kwargs)
     return new
 
+# DATA STRUCTURE
+
+
+class TreeError(Exception):
+    def __init__(self, msg: str, *args, **kwargs):
+        logger.error(msg)
+        super().__init__(msg, *args, **kwargs)
+
+
+class Tree(object):
+
+    def __init__(self, start_statement: str, branch_name: str = 'A', parent: Tree = None, children_l: Tree = None, children_r: Tree = None, leaves_list: tp.Dict[str, Tree] = None):
+        self.name = branch_name
+        self.statements = [start_statement]
+        self.parent = parent
+        assert (children_l and children_r) or (
+            not children_l and not children_r), "One child"
+        self.left = children_l
+        self.right = children_r
+        self.closed = False
+        if leaves_list:
+            leaves_list[branch_name] = self
+        self.leaves = leaves_list
+
+    # Technical
+
+    @EngineLog
+    def _gen_name(self) -> tp.Tuple[str]:
+        if self.parent:
+            dist = 2*alphabet.index(self.name) - (alphabet.index(self.parent.children()[0].name) - alphabet.index(self.parent.children()[1].name)
+            assert dist != 0
+            new=abs(dist)//2
+            if dist < 0:
+                if self.leaves:
+                    assert not alphabet[new] in self.leaves.keys()
+                return self.name, alphabet[new]
+            else:
+                if self.leaves:
+                    assert not alphabet[25-new] in self.leaves.keys()
+                return alphabet[25-new], self.name
+        else:
+            return 'A', 'Z'
+
+    # Tree reading
+
+    def children(self):
+        return self.left, self.right
+
+    def getbranch(self):
+        if self.parent:
+            return self.parent.getbranch() + self.statements
+        else:
+            return self.statements
+
+    def gettree(self):
+        if self.left:  # Trees with only one child aren't supported
+            return self.statements
+        else:
+            return (self.statements, self.left.get_tree(), self.right.get_tree())
+
+    # Tree modification
+
+    @EngineLog
+    def add_statement(self, statement: str):
+        self.statements.append(statement)
+
+    @EngineLog
+    def add_child(self, l_statement: str, r_statement: str):
+        names=self._gen_name()
+        self.left=Tree(l_statement, names[0], self, leaves_list=self.leaves)
+        self.right=Tree(r_statement, names[1], self, leaves_list=self.leaves)
+
+    def append(self, *statements):
+        if len(statements) == 1:
+            self.add_statement(*statements)
+        elif len(statements) == 2:
+            self.add_child(*statements)
+        else:
+            TreeError(
+                f'Trying to append {len(statements)} statements to the tree')
+
+# ENGINE
+
 # Exceptions
 
 
@@ -38,13 +122,13 @@ class EngineError(Exception):
 
 
 class Session(object):
-    ENGINE_VERSION = '0.0.1'
+    ENGINE_VERSION='0.0.1'
 
     def __init__(self, config_file: str):
-        self.config_name = config_file
+        self.config_name=config_file
         self.read_config()
-        self.iu_socket = self.config['chosen_plugins']['UserInterface']
-        self.sockets = {
+        self.iu_socket=self.config['chosen_plugins']['UserInterface']
+        self.sockets={
             'FormalSystem': pop.Socket('FormalSystem', os.path.abspath(
                 'FormalSystem'), '0.0.1', '__template__.py'),
 
@@ -55,15 +139,15 @@ class Session(object):
     @ EngineChangeLog
     def plug_switch(self, socket_or_old: str, new: str) -> None:
         if socket_or_old == 'UserInterface' or socket_or_old == self.config['chosen_plugins']['UserInterface']:
-            socket_name = 'UserInterface'
+            socket_name='UserInterface'
         else:
             # Socket name searching
-            socket = self.sockets.get(socket_or_old, None)
+            socket=self.sockets.get(socket_or_old, None)
             if not socket:
                 for i in self.config['chosen_plugins'].items():
                     if i[1] == socket_or_old:
-                        socket_name = i[0]
-                        socket = self.sockets[socket_name]
+                        socket_name=i[0]
+                        socket=self.sockets[socket_name]
             if not socket:
                 raise EngineError(f"Socket/plugin {socket_or_old} not found")
 
@@ -71,11 +155,11 @@ class Session(object):
             socket.plug(new)
 
         # Config editing
-        self.config['chosen_plugins'][socket.name] = new
+        self.config['chosen_plugins'][socket.name]=new
         self.write_config()
 
     def plug_list(self, socket: str) -> list:
-        sock = self.sockets.get(socket, None)
+        sock=self.sockets.get(socket, None)
         if sock is None:
             if socket == "UserInterface":
                 return [file[:-3] for file in os.listdir("UserInterface") if (
@@ -85,7 +169,7 @@ class Session(object):
             return sock.find_plugins()
 
     def plug_gen(self, socket: str, name: str) -> None:
-        sock = self.sockets.get(socket, None)
+        sock=self.sockets.get(socket, None)
         if sock is None:
             if socket == "UserInterface":
                 raise EngineError(
@@ -99,7 +183,7 @@ class Session(object):
     def read_config(self):
         logger.debug("Config loading")
         with open(self.config_name, 'r') as target:
-            self.config = json.load(target)
+            self.config=json.load(target)
 
     def write_config(self):
         logger.debug("Config writing")
