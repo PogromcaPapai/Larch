3a14d41769a97e56127960d42e5da8b346164746
Jakub Dakowski <jakubdakowski@gmail.com>
2021-03-23 10:40:41 +0100

    Added a crash report generator

--- app/start.py
+++ app/start.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+from datetime import now as dtnow
 
 import pop_engine as pop
 
@@ -18,23 +19,34 @@ import pop_engine as pop
 # For now just do what you can to avoid this.
 
 if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    try:
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    # Log clearing
-    if os.path.exists('log.log'):
-        os.remove('log.log')
+        # Log clearing
+        if os.path.exists('log.log'):
+            os.remove('log.log')
 
-    # UserInterface socket generation
-    UI = pop.Socket('UserInterface', os.path.abspath(
-        'UserInterface'), '0.0.1', '__template__')
+        # UserInterface socket generation
+        UI = pop.Socket('UserInterface', os.path.abspath(
+            'UserInterface'), '0.0.1', '__template__')
 
-    # return exit code -1 to initiate a restart (useful for UI plugin switching)
-    exit_code = -1  # Not implemented in cmd plugin
-    while exit_code == -1:
+        # return exit code -1 to initiate a restart (useful for UI plugin switching)
+        exit_code = -1  # Not implemented in cmd plugin
+        while exit_code == -1:
 
-        # App run
-        with open('config.json', 'r') as file:
-            config = json.load(file)
-        UI.plug(config['chosen_plugins']['UserInterface'])
-        exit_code = UI().run()
-    sys.exit(exit_code)
+            # App run
+            with open('config.json', 'r') as file:
+                config = json.load(file)
+            UI.plug(config['chosen_plugins']['UserInterface'])
+            exit_code = UI().run()
+        sys.exit(exit_code)
+
+    # Crash report generator
+
+    except BaseException as e:
+        with open('log.log', 'r') as l:
+            logs = l.read()
+        with open(f'crashreport-{dtnow().strftime("%d-%m-%Y-%H-%M")}.txt', 'w') as f:
+            f.write(logs)
+            f.write('\nEXCEPTION:\n')
+            f.write(str(e))
